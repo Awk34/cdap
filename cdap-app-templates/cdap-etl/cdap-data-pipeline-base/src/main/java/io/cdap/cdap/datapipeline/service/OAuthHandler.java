939242d2f6bc71e6b0f8af1cde93bca38ec5ba6f
Andrew Koroluk <koroluka@google.com>
2025-06-06 00:05:19 -0400

    Allow saving a new refresh token sent from OAuth APIs when requesting access tokens

--- cdap-app-templates/cdap-etl/cdap-data-pipeline-base/src/main/java/io/cdap/cdap/datapipeline/service/OAuthHandler.java
+++ cdap-app-templates/cdap-etl/cdap-data-pipeline-base/src/main/java/io/cdap/cdap/datapipeline/service/OAuthHandler.java
@@ -45,6 +45,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Objects;
 import java.util.Optional;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -264,9 +265,29 @@ public class OAuthHandler extends AbstractSystemHttpServiceHandler {
       } catch (JsonSyntaxException e) {
         throw new OAuthServiceException(HttpURLConnection.HTTP_INTERNAL_ERROR, "Error parsing JSON response", e);
       }
-      if (refreshTokenResponse.getAccessToken() == null || refreshTokenResponse.getAccessToken().isEmpty()) {
+
+      boolean hasRefreshToken = refreshTokenResponse.getRefreshToken() != null
+          && !refreshTokenResponse.getRefreshToken().isEmpty();
+      boolean hasAccessToken = refreshTokenResponse.getAccessToken() != null
+          && !refreshTokenResponse.getAccessToken().isEmpty();
+
+      if (!hasAccessToken) {
         throw new OAuthServiceException(
-            HttpURLConnection.HTTP_INTERNAL_ERROR, "Refresh token response body does not have refresh token");
+            HttpURLConnection.HTTP_INTERNAL_ERROR, "Access token response body does not have access token");
+      }
+
+      // API has given us a new refresh token
+      if (hasRefreshToken && !refreshToken.getRefreshToken().equals(refreshTokenResponse.getRefreshToken())) {
+        OAuthRefreshToken newRefreshToken = OAuthRefreshToken.newBuilder()
+                .withRefreshToken(refreshTokenResponse.getRefreshToken())
+                .withRedirectURI(refreshToken.getRedirectURI())
+                .build();
+
+        try {
+          oauthStore.writeRefreshToken(provider, credentialId, newRefreshToken);
+        } catch (OAuthStoreException e) {
+          LOG.error("An error occurred while writing the new refresh token", e);
+        }
       }
 
       responder.sendString(GSON.toJson(
